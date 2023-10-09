1e66db3b9f13e3aaec3d5ad2d9070ee146b4f4a2
choideakook <shdrnrhd113@naver.com>
2023-10-09 19:33:45 +0900

    docs: application yml 라우팅 추가

--- src/main/java/com/example/gateway/global/constants/Address.java
+++
@@ -1,6 +0,0 @@
-package com.example.gateway.global.constants;
-
-public class Address {
-    public static final String AUTH_URL = "/oauth2/authorization/kakao";
-    public static final String LOGIN_URL = "/login";
-}

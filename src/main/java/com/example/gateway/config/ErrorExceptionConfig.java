c8cde827b4249e32e0807bbeb06eb0ba7186d2ec
choideakook <shdrnrhd113@naver.com>
2023-11-14 12:11:58 +0900

    etx: exception handler 비활성화

--- src/main/java/com/example/gateway/config/ErrorExceptionConfig.java
+++ src/main/java/com/example/gateway/config/ErrorExceptionConfig.java
@@ -1,18 +1,18 @@
 package com.example.gateway.config;
 
-import com.example.gateway.filter.GlobalExceptionHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class ErrorExceptionConfig {
-    private final ObjectMapper objectMapper;
-    @Bean
-    public ErrorWebExceptionHandler globalExceptionHandler() {
-        return new GlobalExceptionHandler(objectMapper);
-    }
-}
+//import com.example.gateway.filter.GlobalExceptionHandler;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class ErrorExceptionConfig {
+//    private final ObjectMapper objectMapper;
+//    @Bean
+//    public ErrorWebExceptionHandler globalExceptionHandler() {
+//        return new GlobalExceptionHandler(objectMapper);
+//    }
+//}
--- src/main/java/com/example/gateway/filter/GlobalExceptionHandler.java
+++ src/main/java/com/example/gateway/filter/GlobalExceptionHandler.java
@@ -1,49 +1,49 @@
 package com.example.gateway.filter;
 
-import com.example.gateway.global.error.exception.GatewayErrorResponse;
-import com.example.gateway.global.error.exception.token.TokenValidException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-@Slf4j
-@Order(-1)
-@RequiredArgsConstructor
-public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
-    private final ObjectMapper objectMapper;
-
-    @Override
-    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
-        ServerHttpResponse response = exchange.getResponse();
-
-        if (response.isCommitted()) {
-            return Mono.error(ex);
-        }
-
-        // Header
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        if (ex instanceof TokenValidException) {
-            response.setStatusCode(((HttpStatus.UNAUTHORIZED).is4xxClientError()) ? HttpStatus.UNAUTHORIZED : HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return response.writeWith(Mono.fromSupplier(() -> {
-            DataBufferFactory bufferFactory = response.bufferFactory();
-            try {
-                GatewayErrorResponse gwErrorResponse = GatewayErrorResponse.defaultError(ex.getMessage());
-                byte[] errorResponse = objectMapper.writeValueAsBytes(gwErrorResponse);
-
-                return bufferFactory.wrap(errorResponse);
-            } catch (Exception e) {
-                log.error("error", e);
-                return bufferFactory.wrap(new byte[0]);
-            }
-        }));
-    }
-}
+//import com.example.gateway.global.error.exception.GatewayErrorResponse;
+//import com.example.gateway.global.error.exception.token.TokenValidException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.core.io.buffer.DataBufferFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Slf4j
+//@Order(-1)
+//@RequiredArgsConstructor
+//public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+//    private final ObjectMapper objectMapper;
+//
+//    @Override
+//    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+//        ServerHttpResponse response = exchange.getResponse();
+//
+//        if (response.isCommitted()) {
+//            return Mono.error(ex);
+//        }
+//
+//        // Header
+//        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+//        if (ex instanceof TokenValidException) {
+//            response.setStatusCode(((HttpStatus.UNAUTHORIZED).is4xxClientError()) ? HttpStatus.UNAUTHORIZED : HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//        return response.writeWith(Mono.fromSupplier(() -> {
+//            DataBufferFactory bufferFactory = response.bufferFactory();
+//            try {
+//                GatewayErrorResponse gwErrorResponse = GatewayErrorResponse.defaultError(ex.getMessage());
+//                byte[] errorResponse = objectMapper.writeValueAsBytes(gwErrorResponse);
+//
+//                return bufferFactory.wrap(errorResponse);
+//            } catch (Exception e) {
+//                log.error("error", e);
+//                return bufferFactory.wrap(new byte[0]);
+//            }
+//        }));
+//    }
+//}
